9c4468eb52fac7ef2248a69cc355af53db49130b
Eric Alfred <alfrederic371@gmail.com>
2025-10-09 23:12:00 +0100

    chore: added social media links

--- components/Footer.vue
+++ components/Footer.vue
@@ -21,13 +21,13 @@
               </h2>
               <ul class="text-gray-600 ">
                 <li class="mb-4">
-                  <NuxtLink href="#Features" class="hover:underline">Features</NuxtLink>
+                  <NuxtLink href="/features" class="hover:underline">Features</NuxtLink>
                 </li>
                 <li class="mb-4">
-                  <NuxtLink href="#Stories" class="hover:underline">Stories</NuxtLink>
+                  <NuxtLink href="/stories" class="hover:underline">Stories</NuxtLink>
                 </li>
                 <li>
-                  <NuxtLink href="#Pricing" class="hover:underline">Pricing</NuxtLink>
+                  <NuxtLink href="/pricing" class="hover:underline">Pricing</NuxtLink>
                 </li>
               </ul>
             </div>
@@ -39,10 +39,10 @@
               </h2>
               <ul class="text-gray-600 ">
                 <li class="mb-4">
-                  <NuxtLink href="#" class="hover:underline">X (Twitter)</NuxtLink>
+                  <a href="https://x.com/mytransactx" target="_blank" rel="noopener noreferrer" class="hover:underline">X (Twitter)</a>
                 </li>
                 <li>
-                  <NuxtLink href="#" class="hover:underline">Instagram</NuxtLink>
+                  <a href="https://www.instagram.com/mytransactx/" target="_blank" rel="noopener noreferrer" class="hover:underline">Instagram</a>
                 </li>
               </ul>
             </div>
@@ -67,7 +67,7 @@
              All Rights Reserved.
           </span>
           <div class="flex mt-4 space-x-5 sm:justify-center sm:mt-0">
-            <NuxtLink href="#" class="text-gray-500 hover:text-gray-900">
+            <a href="https://x.com/mytransactx" target="_blank" rel="noopener noreferrer" class="text-gray-500 hover:text-gray-900">
               <svg
                 class="w-8 h-8"
                 fill="currentColor"
@@ -75,13 +75,11 @@
                 aria-hidden="true"
               >
                 <path
-                  fill-rule="evenodd"
-                  d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z"
-                  clip-rule="evenodd"
+                  d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
                 />
               </svg>
-            </NuxtLink>
-            <NuxtLink href="#" class="text-gray-500 hover:text-gray-900">
+            </a>
+            <a href="https://www.instagram.com/mytransactx/" target="_blank" rel="noopener noreferrer" class="text-gray-500 hover:text-gray-900">
               <svg
                 class="w-8 h-8"
                 fill="currentColor"
@@ -94,19 +92,7 @@
                   clip-rule="evenodd"
                 />
               </svg>
-            </NuxtLink>
-            <NuxtLink href="#" class="text-gray-500 hover:text-gray-900">
-              <svg
-                class="w-8 h-8"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-                aria-hidden="true"
-              >
-                <path
-                  d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
-                />
-              </svg>
-            </NuxtLink>
+            </a>
           </div>
         </div>
       </div>
